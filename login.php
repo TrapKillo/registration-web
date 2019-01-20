4ff83c15cbcd0794a94ec477171f38bfcc8f0ac4
MrTrap.js <killotrap@gmail.com>
2019-01-20 09:50:32 -1100

    Update login.php

--- login.php
+++ login.php
@@ -4,16 +4,21 @@
 <html>
 	<head>
 		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="styles.css"/>
+		<script src="script.js"></script>
 	</head>
 <body>
-	<center>
-		<h2>Login</h2>
-		<form action="login.php" method="post">
-			<?php include('errors.php'); ?>
-			<input type="text" name="username" class="form__input" placeholder="username"/><br style="line-height:25px;">
-			<input type="password" name="password" class="form__input" placeholder="password"/><br style="line-height:25px;">
-			<button type="submit" class="btn" name="login">Login</button> <p>Don't have an account? <a style="color: black; text-decoration:none; a:visited { text-decoration: none; color:red; } a:hover { text-decoration: none; color:blue; } a:focus { text-decoration: none; color:yellow; } a:hover, a:active { text-decoration: none; color:black }" href="register.php">Register</a> </p>
-		</form>
-	</center>
+	<div class="wrapper">
+		<center>
+			<form action="login.php" method="post" class="form">
+				<h1>Login</h1>
+				<?php include('errors.php'); ?>
+				<input type="text" name="username" autocomplete="off" placeholder="username" /><br style="line-height:25px;">
+				<input type="password" name="password" placeholder="password"/><br style="line-height:25px;">
+					<input type="submit" onclick="animateLogin();" class="button" id='loginBTN' name="login" value="Login">
+					<p>Don't have an account? <a href="register.php">Register</a> </p>
+			</form>
+		</center>
+	</div>
 </body>
 </html>
