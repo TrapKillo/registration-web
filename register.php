2d8579b0aef4d95f7ab1dea7570d4294c7c5161a
MrTrap.js <killotrap@gmail.com>
2019-01-20 09:50:17 -1100

    Update register.php

--- register.php
+++ register.php
@@ -4,19 +4,23 @@
 <html>
 	<head>
 		<title>Register</title>
+		<link rel="stylesheet" type="text/css" href="styles.css"/>
+		<script src="script.js"></script>
 	</head>
 <body>
-	<center>
-		<h2>Register</h2>
-		<form method="post" action="register.php">
-			<?php include('errors.php'); ?>
-			<input type="text" name="username" placeholder="username" value="<?php echo $username; ?>" /><br style="line-height:25px;">
-			<input type="text" name="email" placeholder="email" value="<?php echo $email; ?>"><br style="line-height:25px;">
-			<input type="password" name="password1" placeholder="password"/><br style="line-height:25px;">
-			<input type="password" name="password2" placeholder="confirm password"/><br style="line-height:25px;">
-			<input type="submit" name="register" value="Register"/><p>Already have an account?
-				<a style="color: black; text-decoration:none; a:visited { text-decoration: none; color:red; } a:hover { text-decoration: none; color:blue; } a:focus { text-decoration: none; color:yellow; } a:hover, a:active { text-decoration: none; color:black }" href="login.php">Login</a> </p>
-		</form>
-	</center>
+	<div class="wrapper">
+		<center>
+			<form method="post" action="register.php" class="form">
+				<h1>Register</h1>
+				<?php include('errors.php'); ?>
+				<input type="text" name="username" autocomplete="off" placeholder="username" value="<?php echo $username; ?>" /><br style="line-height:25px;">
+				<input type="text" name="email" autocomplete="off" placeholder="email" value="<?php echo $email; ?>"><br style="line-height:25px;">
+				<input type="password" name="password1" placeholder="password"/><br style="line-height:25px;">
+				<input type="password" name="password2" placeholder="confirm password"/><br style="line-height:25px;">
+				<input type="submit" onclick="animateRegister();" class="button" id='registerBTN' name="register" value="Register">
+				<p> Already have an account? <a href="login.php">Login</a> </p>
+			</form>
+		</center>
+	</div>
 </body>
 </html>
